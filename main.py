53f6faf580c5342eb9f5aced51ddfd47349ff7ed
Marcelo Portilho <marceloport@gmail.com>
2021-05-04 21:27:42 -0300

    pre-validated schema generation

--- main.py
+++ main.py
@@ -6,14 +6,36 @@ import yaml
 from processors import schema_processor
 
 
-def execute_program():
-    openbk_swagger_path = Path(
-        os.path.join(os.path.expanduser("~"), "temp", "ram", "email openbk hml", "swagger_channels_apis.yaml"))
+def _load_yaml(openbk_swagger_path: Path):
     if not openbk_swagger_path.exists():
         raise Exception(f'Source file "{openbk_swagger_path}" was not found')
     with open(openbk_swagger_path, 'r', encoding='utf8') as file:
-        openapi_obk = yaml.safe_load(file)
-    schema_processor.process(openapi_obk)
+        yaml_obj = yaml.safe_load(file)
+    return yaml_obj
+
+
+def execute_program():
+    openapi_obk = _load_yaml(Path(
+        os.path.join(os.path.expanduser("~"), "temp", "ram", "email openbk hml", "swagger_common_apis.yaml")))
+    openapi_api = _load_yaml(Path(
+        os.path.join(os.path.expanduser("~"), "temp", "ram", "email openbk hml", "api-comuns.yaml")))
+
+    # openapi_obk = _load_yaml(Path(
+    #     os.path.join(os.path.expanduser("~"), "temp", "ram", "email openbk hml", "swagger_channels_apis.yaml")))
+    # openapi_api = _load_yaml(Path(
+    #     os.path.join(os.path.expanduser("~"), "temp", "ram", "email openbk hml", "api-canais-atendimento.yaml")))
+
+    # openapi_obk = _load_yaml(Path(
+    #     os.path.join(os.path.expanduser("~"), "temp", "ram", "email openbk hml", "swagger_channels_apis.yaml")))
+    # openapi_api = _load_yaml(Path(
+    #     os.path.join(os.path.expanduser("~"), "temp", "ram", "email openbk hml", "api-canais-atendimento.yaml")))
+
+    # api-canais-atendimento.yaml
+    schema_obk: dict = schema_processor.process(openapi_obk)
+    schema_api: dict = schema_processor.process(openapi_api)
+
+    print(schema_obk)
+    print(schema_api)
 
 
 if __name__ == '__main__':
--- processors/schema_processor.py
+++ processors/schema_processor.py
@@ -1,37 +1,29 @@
 from openapi3_structs.schema import Schema
 
 
-def process(openapi):
+def process(openapi) -> dict:
     component_schemas = openapi['components']['schemas']
     cache: dict = {}
-
     for schema_key in component_schemas:
         cache[schema_key] = _create_new_schema(schema_key, component_schemas[schema_key])
-
     for schema_key in component_schemas:
         _process_schema(cache[schema_key], component_schemas[schema_key], cache)
+    return cache
 
-    print(cache)
 
-
-def _process_schema(schema: Schema, schema_source: dict, cache: dict):
-    print(schema_source)
+def _process_schema(schema: Schema, schema_source: dict, cache: dict) -> dict:
     if '$ref' in schema_source:
         key = schema['$ref'].removeprefix('#/components/schemas/')
         schema['$$_ref'] = cache[key]
-        return cache[key]
     elif 'properties' in schema_source:
         for k in schema_source['properties']:
             property_schema = _create_new_schema(k, schema_source['properties'][k])
             schema['properties'][k] = _process_schema(property_schema, schema_source['properties'][k], cache)
-        return schema
     elif 'type' in schema_source and schema_source['type'] == 'array':
         if 'items' not in schema_source:
             raise Exception(f'Schema property defined as array but no "items" property attribute found ')
         array_schema = _create_new_schema('array_item', schema_source['items'])
         schema['items'] = _process_schema(array_schema, schema_source['items'], cache)
-    # elif schema_source['type'] == 'object':
-    #     raise Exception(f'Schema type is object but no "$ref" or "properties" attributes defined')
 
     if 'additionalProperties' in schema_source:
         if schema_source['additionalProperties'] is True or schema_source['additionalProperties'] == 'true':
@@ -41,21 +33,14 @@ def _process_schema(schema: Schema, schema_source: dict, cache: dict):
             schema['additionalProperties'] = additional_schema
             return schema
 
-    if 'allOf' in schema_source:
-        all_of_schema = _create_new_schema('allOf', schema_source['allOf'])
-        schema['$$_allOf'] = _process_schema(all_of_schema, schema_source['allOf'], cache)
-
-    if 'oneOf' in schema_source:
-        one_of_schema = _create_new_schema('oneOf', schema_source['oneOf'])
-        schema['$$_oneOf'] = _process_schema(one_of_schema, schema_source['oneOf'], cache)
-
-    if 'anyOf' in schema_source:
-        any_of_schema = _create_new_schema('anyOf', schema_source['anyOf'])
-        schema['$$_anyOf'] = _process_schema(any_of_schema, schema_source['anyOf'], cache)
-
-    if 'not' in schema_source:
-        not_of_schema = _create_new_schema('not', schema_source['not'])
-        schema['$$_not'] = _process_schema(not_of_schema, schema_source['not'], cache)
+    dynamic_attr_list = ['allOf', 'oneOf', 'anyOf', 'not']
+    for attr in dynamic_attr_list:
+        if attr in schema_source:
+            attr_array: list = []
+            for each_schema in schema_source[attr]:
+                all_of_schema = _create_new_schema(attr, each_schema)
+                attr_array.append(_process_schema(all_of_schema, each_schema, cache))
+            schema[f'$$_{attr}'] = attr_array
 
     return schema
 
